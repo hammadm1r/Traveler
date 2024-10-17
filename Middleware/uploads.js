7c7bc1f943597cf0edadf3cca273cbfc81671d61
Hammad774 <hammad24197@gmail.com>
2024-10-17 14:07:53 +0500

    changes

---
+++ Middleware/uploads.js
@@ -0,0 +1,20 @@
+const multer  = require('multer')
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/products')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+
+  const upload = multer({ storage: storage ,
+    limits: {
+        // limits file size to 5 MB
+        fileSize: 1024 * 1024 * 5
+    }
+    // ,fileFilter: fileFilterConfig,
+});
+  module.exports = upload;
--- Routers/authenticationRouters.js
+++ Routers/authenticationRouters.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const {signup,login,getProfile} = require('../Controllers/authenticationController')
 const {verifyAuthToken} = require('../Middleware/jwtAuthMiddleware')
+const upload = require('../Middleware/uploads')
 
-router.post('/signup',signup);
+router.post('/signup',upload.single('profilePicture'),signup);
 router.post('/login',login);
 router.get('/profile',verifyAuthToken,getProfile);
 module.exports = router;
