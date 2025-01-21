d30067bcc32111d7afd66365d53a29fb7819bb9c
Hammad774 <hammad24197@gmail.com>
2025-01-21 23:54:51 +0500

    changes

--- Controllers/storyController.js
+++ Controllers/storyController.js
@@ -6,50 +6,17 @@ const { mapPostOutput } = require("../Utils/utils");
 const express = require("express");
 const cloudinary = require("../Utils/cloudinaryConfig");
 const dotenv = require("dotenv");
+const Story = require("../Models/story");
 
 dotenv.config();
 
 const addStory = async (req, res) => {
   try {
-    const { title, lat, long } = req.body;
+    console.log(req.body);
+    const { title, lat, long , url, publicId} = req.body;
 
-    if (!title || !lat || !long) {
+    if (!title || !lat || !long || !url || !publicId) {
       return res.send(error(400, "All fields are required"));
-    }
-
-    if (!req.file) {
-      return res.send(error(400, "Story media is required"));
-    }
-
-    console.log("Received File");
-
-    const resourceType = req.file.mimetype.startsWith("video/") ? "video" : "image";
-
-    let media;
-    try {
-      const uploadPromise = new Promise((resolve, reject) => {
-        const stream = cloudinary.uploader.upload_stream(
-          {
-            folder: "Story_Media",
-            resource_type: resourceType,
-          },
-          (error, result) => {
-            if (error) {
-              return reject({
-                message: "Upload to Cloudinary failed",
-                error,
-              });
-            }
-            resolve({ public_id: result.public_id, url: result.secure_url });
-          }
-        );
-        stream.end(req.file.buffer);
-      });
-
-      media = await uploadPromise;
-      console.log("Uploaded media details:", media);
-    } catch (uploadError) {
-      return res.send(error(500, "Failed to upload media"));
     }
 
     if (!req.user || !req.user.user_Id) {
@@ -65,7 +32,10 @@ const addStory = async (req, res) => {
       title,
       location: { latitude: lat, longitude: long },
       userId: req.user.user_Id,
-      video: media, // Save media object directly
+      video : {
+        url,
+        publicId
+      }
     });
 
     author.stories.push(newStory._id);
@@ -95,7 +65,14 @@ const generateSignature = (req,res) => {
     cloudName: process.env.CLOUD_NAME,
     apiKey: process.env.API_KEY,
   });
-  return res.json(success(201, { data}));
+  return res.json(success(201, {data}));
 }
 
-module.exports = { addStory,generateSignature };
+const getStory = async(req,res) => {
+  const allStory = await story.find();
+  console.log(allStory);
+  return res.json(success(201,{allStory}));
+}
+
+
+module.exports = { addStory,generateSignature,getStory };
--- Routers/storyRouter.js
+++ Routers/storyRouter.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
-const {addStory,generateSignature} = require('../Controllers/storyController')
+const {addStory,generateSignature,getStory} = require('../Controllers/storyController')
 const {verifyAuthToken} = require('../Middleware/jwtAuthMiddleware')
 const { storyUpload } = require('../Middleware/uploads');
 
-router.post('/addstory',verifyAuthToken,storyUpload,addStory);
+router.post('/addstory',verifyAuthToken,addStory);
 router.get('/generate-signature',generateSignature),
-
+router.get('/getstory',verifyAuthToken,getStory);
 module.exports = router;
